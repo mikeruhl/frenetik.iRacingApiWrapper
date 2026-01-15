b4bd45f8f34ab72ff07de350225d779233211b50
Mike Ruhl <mikeruhl@me.com>
2026-01-15 08:14:23 -0700

    enum and converter doc

--- Frenetik.iRacingApiWrapper/Enums/LeagueSortValue.cs
+++ Frenetik.iRacingApiWrapper/Enums/LeagueSortValue.cs
@@ -2,14 +2,29 @@
 
 namespace Frenetik.iRacingApiWrapper.Enums;
 
+/// <summary>
+/// Values for sorting league search results.
+/// </summary>
 public enum LeagueSortValue
 {
+    /// <summary>
+    /// Sort by relevance to the search query.
+    /// </summary>
     [EnumMember(Value = "relevance")]
     Relevance,
+    /// <summary>
+    /// Sort by league name.
+    /// </summary>
     [EnumMember(Value = "leaguename")]
     LeagueName,
+    /// <summary>
+    /// Sort by display name.
+    /// </summary>
     [EnumMember(Value = "displayname")]
     DisplayName,
+    /// <summary>
+    /// Sort by the number of members in the roster.
+    /// </summary>
     [EnumMember(Value = "rostercount")]
     RosterCount
 }
--- Frenetik.iRacingApiWrapper/Enums/SortOrder.cs
+++ Frenetik.iRacingApiWrapper/Enums/SortOrder.cs
@@ -1,10 +1,19 @@
 ﻿using System.Runtime.Serialization;
 
 namespace Frenetik.iRacingApiWrapper.Enums;
+/// <summary>
+/// Sort order direction.
+/// </summary>
 public enum SortOrder
 {
+    /// <summary>
+    /// Ascending order (lowest to highest).
+    /// </summary>
     [EnumMember(Value = "asc")]
     Asc,
+    /// <summary>
+    /// Descending order (highest to lowest).
+    /// </summary>
     [EnumMember(Value = "desc")]
     Desc
 }
